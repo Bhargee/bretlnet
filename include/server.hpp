c006e40b54b34f908076438c1b3e5f623b862bf7
Bhargava Manja <bhargee.manja@gmail.com>
2015-05-22 16:40:43 -0500

    Changed server structure, more modular but no TCP init

--- include/server.hpp
+++ include/server.hpp
@@ -2,16 +2,25 @@
 #define BRETLNET_SERVER_H
 
 #include "common.hpp"
+//#include "threadpool.hpp"
 
 class Server : private BretlNetService {
     public:
         enum Protocol {TCP, UDP};
-        Server(Protocol p, int portNum, int buflen);
+        Server(Protocol p, int portNum, int numThreads);
         ~Server();
         void Serve(void (*f)(char *));
     private:
-        int bufsize;
         Protocol proto;
+        int nThreads;
+//        ThreadPool workerPool;
+        // do socket init (socket, bind, listen)
+        void InitUDP();
+        void InitTCP();
+        void Init();
+        // pthread listener?
 };
+
+
 
 #endif
--- src/server.cpp
+++ src/server.cpp
@@ -1,44 +1,46 @@
 #include "server.hpp"
 
-Server::Server(Protocol p, int portNum, int buflen) : BretlNetService(portNum) {
-    this->bufsize = (p != Protocol::TCP) ? std::min(buflen, (2 << 16) - 8) : buflen;
+Server::Server(Protocol p, int portNum, int numThreads) 
+    : BretlNetService(portNum) {
     this->proto = p;
+    this->nThreads = numThreads;
 }
 
 Server::~Server() {
 }
 
-void Server::Serve(void (*f)(char *)) {
-    // TODO add support for TCP
-    // TODO make async
+// might throw exception
+void Server::Init() {
+    (this->proto == UDP) ? InitUDP() : InitTCP();
+}
+
+void Server::InitTCP() {
+}
+
+void Server::InitUDP() {
     struct addrinfo hints, *servinfo, *p;
     int rv;
-    int numbytes;
-    struct sockaddr_storage their_addr;
-    socklen_t addr_len;
+    const char *error_msg;
 
     memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC; // set to AF_INET to force IPv4
+    hints.ai_family = AF_UNSPEC; 
     hints.ai_socktype = SOCK_DGRAM;
-    hints.ai_flags = AI_PASSIVE; // use my IP
+    hints.ai_flags = AI_PASSIVE;
 
     std::string port_str = std::to_string(this->port);
     if ((rv = getaddrinfo(NULL, port_str.c_str(), &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        //return 1;
+        error_msg = gai_strerror(rv);
+        goto error;
     }
 
-    // loop through all the results and bind to the first we can
     for(p = servinfo; p != NULL; p = p->ai_next) {
         if ((sockfd = socket(p->ai_family, p->ai_socktype,
                 p->ai_protocol)) == -1) {
-            perror("listener: socket");
             continue;
         }
 
         if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
             close(sockfd);
-            perror("listener: bind");
             continue;
         }
 
@@ -46,21 +48,16 @@ void Server::Serve(void (*f)(char *)) {
     }
 
     if (p == NULL) {
-        fprintf(stderr, "listener: failed to bind socket\n");
-        //return 2;
+        error_msg = "failed to bind socket\n";
+        goto error;
     }
 
     freeaddrinfo(servinfo);
+    return;
 
-    char buf[this->bufsize];
+error:
+    throw std::runtime_error(error_msg);
+}
 
-    addr_len = sizeof their_addr;
-    while(1) {
-        if ((numbytes = recvfrom(this->sockfd, buf, this->bufsize , 0,
-            (struct sockaddr *)&their_addr, &addr_len)) == -1) {
-            perror("recvfrom");
-            exit(1);
-        } 
-        f(buf);
-    }
+void Server::Serve(void (*f)(char *)) {
 }
